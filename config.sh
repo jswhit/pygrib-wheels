5fe68abd1afae2b5828d7976cdc7e59791ab507e
Jeff Whitaker <jswhit@fastmail.fm>
2020-12-21 18:56:42 -0700

    update

--- config.sh
+++ config.sh
@@ -1,12 +1,6 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
-function pre_build {
-    # Any stuff that you need to do before you start building the wheels
-    # Runs in the root directory of this repository.
-    :
-}
-
 # Compile libs for macOS 10.9 or later
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 export ECCODES_VERSION="2.19.1"
@@ -27,9 +21,6 @@ function build_libs {
 }
 
 function pre_build {
-    echo "in pre_build"
-    pwd
-    ls -l 
     export PYGRIB_DIR=$PWD/pygrib
     build_libs
 }
@@ -40,7 +31,7 @@ function build_eccodes {
     build_openjpeg
     build_libaec
     fetch_unpack https://confluence.ecmwf.int/download/attachments/45757960/eccodes-${ECCODES_VERSION}-Source.tar.gz
-    /bin/cp -r ecodes-${ECCODES_VERSION}-Source/eccodes/definitions/ $PYGRIB_DIR/eccodes
+    /bin/cp -r eccodes-${ECCODES_VERSION}-Source/eccodes/definitions/ $PYGRIB_DIR/eccodes
     mkdir build
     cd build
     cmake -DENABLE_FORTRAN=OFF -DENABLE_NETCDF=OFF -DENABLE_TESTS=OFF -DENABLE_JPG_LIBJASPER=OFF -DENABLE_JPG_LIBOPENJPEG=ON -DENABLE_PNG=ON -DENABLE_AEC=ON ../eccodes-${ECCODES_VERSION}-Source
