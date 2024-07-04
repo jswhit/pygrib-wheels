057cc4d1124c98e2483b978aee6bf8124dc00c91
Jeff Whitaker <jswhit@fastmail.fm>
2024-07-04 15:29:30 -0600

    update

--- config.sh
+++ config.sh
@@ -2,13 +2,13 @@
 # Test for OSX with [ -n "$IS_OSX" ]
 
 export ECCODES_VERSION="2.33.0"
-export OPENJPEG_VERSION="2.4.0"
-export PNG_VERSION="1.6.37"
-export ZLIB_VERSION="1.2.11"
-export LIBAEC_VERSION="1.0.6"
+export OPENJPEG_VERSION="2.5.2"
+export PNG_VERSION="1.6.43"
+export ZLIB_VERSION="1.3.1"
+export LIBAEC_VERSION="1.1.3"
 export PYGRIB_WHEEL=true
 
-functio nbuild_simple {
+function nbuild_simple {
     # Example: build_simple libpng $LIBPNG_VERSION \
     #               https://download.sourceforge.net/libpng tar.gz \
     #               --additional --configure --arguments
